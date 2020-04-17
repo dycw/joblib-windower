a5b480caba35db32cabec2e52da8177d39bcab44
Derek Wan <d.wan@icloud.com>
2020-04-18 03:21:05 +0800

    Allow customization of the temp_dir

--- joblib_windower/joblib_windower.py
+++ joblib_windower/joblib_windower.py
@@ -5,6 +5,7 @@ from functools import wraps
 from itertools import chain
 from multiprocessing import cpu_count
 from pathlib import Path
+from tempfile import gettempdir
 from tempfile import TemporaryDirectory
 from typing import Any
 from typing import Callable
@@ -25,6 +26,7 @@ from numpy import ndarray
 
 
 _CPU_COUNT = cpu_count()
+_TEMP_DIR = gettempdir()
 T = TypeVar("T")
 
 
@@ -61,81 +63,90 @@ def _maybe_memmap(x: Any, path: Union[Path, str]) -> Any:
         return x
 
 
-def windower(func: Callable[..., Union[float, ndarray]]) -> Callable[..., Union[float, ndarray]]:
-    @wraps(func)
-    def wrapped(
-        *args: Any,
-        window: int = 1,
-        min_frac: Optional[float] = None,
-        n_jobs: int = _CPU_COUNT,
-        **kwargs: Any,
-    ) -> ndarray:
-        if window <= 0:
-            raise ValueError(f"Expected window to be positive; got {window}")
+def windower(
+    func: Optional[Callable[..., Union[float, ndarray]]] = None,
+    *,
+    temp_dir: Union[Path, str] = _TEMP_DIR,
+) -> Callable[..., Union[float, ndarray]]:
 
-        applies_slice = _applies_slice(func)
+    if func is None:
+        return partial(windower, temp_dir=temp_dir)
+    else:
 
-        with TemporaryDirectory() as td:
-            td = Path(td)
+        @wraps(func)
+        def wrapped(
+            *args: Any,
+            window: int = 1,
+            min_frac: Optional[float] = None,
+            n_jobs: int = _CPU_COUNT,
+            **kwargs: Any,
+        ) -> ndarray:
+            if window <= 0:
+                raise ValueError(f"Expected window to be positive; got {window}")
 
-            # maybe memmap
-            new_args: CList[Any] = (
-                CList(args)
-                .enumerate()
-                .starmap(lambda i, v: _maybe_memmap(v, td.joinpath(f"arg_{i}")))
-            )
-            new_kwargs: CDict[str, Any] = CDict(kwargs).map_items(
-                lambda k, v: (k, _maybe_memmap(v, td.joinpath(f"kwarg_{k}"))),
-            )
-            length = (
-                new_args.chain(new_kwargs.values())
-                .filter(lambda x: isinstance(x, memmap))
-                .map(len)
-                .set()
-                .one()
-            )
-            if length == 0:
-                raise ValueError("Expected non-zero length")
+            applies_slice = _applies_slice(func)
 
-            # create indexers
-            if window == 1:
-                indexers: CList[int] = CList.range(length)
-            else:
-                indexers: CList[Optional[slice]] = CList.range(1, length + 1).map(
-                    lambda x: slice(max(x - window, 0), x),
+            with TemporaryDirectory(dir=temp_dir) as td:
+                td = Path(td)
+
+                # maybe memmap
+                new_args: CList[Any] = (
+                    CList(args)
+                    .enumerate()
+                    .starmap(lambda i, v: _maybe_memmap(v, td.joinpath(f"arg_{i}")))
                 )
-                if min_frac is None:
-                    indexers: CList[Optional[slice]] = indexers
+                new_kwargs: CDict[str, Any] = CDict(kwargs).map_items(
+                    lambda k, v: (k, _maybe_memmap(v, td.joinpath(f"kwarg_{k}"))),
+                )
+                length = (
+                    new_args.chain(new_kwargs.values())
+                    .filter(lambda x: isinstance(x, memmap))
+                    .map(len)
+                    .set()
+                    .one()
+                )
+                if length == 0:
+                    raise ValueError("Expected non-zero length")
+
+                # create indexers
+                if window == 1:
+                    indexers: CList[int] = CList.range(length)
                 else:
-                    min_length = min_frac * window
-                    indexers = indexers.map(
-                        lambda x: x if (x.stop - x.start) >= min_length else None,
+                    indexers: CList[Optional[slice]] = CList.range(1, length + 1).map(
+                        lambda x: slice(max(x - window, 0), x),
                     )
+                    if min_frac is None:
+                        indexers: CList[Optional[slice]] = indexers
+                    else:
+                        min_length = min_frac * window
+                        indexers = indexers.map(
+                            lambda x: x if (x.stop - x.start) >= min_length else None,
+                        )
 
-            # compute last slice
-            *_, last_indexer = indexers
-            if last_indexer is None:
-                raise ValueError("Expected the last indexer to be a slice")
-            last_result = applies_slice(*new_args, _indexer=last_indexer, **new_kwargs)
-            last_array = array(last_result)
-            output = memmap(
-                filename=str(td.joinpath("output")),
-                dtype=last_array.dtype,
-                mode="w+",
-                shape=tuple(chain([length], last_array.shape)),
-            )
-            Parallel(n_jobs=n_jobs)(
-                delayed(_writes_output(applies_slice))(
-                    *new_args, _indexer=indexer, _output=output, _index=index, **new_kwargs,
+                # compute last slice
+                *_, last_indexer = indexers
+                if last_indexer is None:
+                    raise ValueError("Expected the last indexer to be a slice")
+                last_result = applies_slice(*new_args, _indexer=last_indexer, **new_kwargs)
+                last_array = array(last_result)
+                output = memmap(
+                    filename=str(td.joinpath("output")),
+                    dtype=last_array.dtype,
+                    mode="w+",
+                    shape=tuple(chain([length], last_array.shape)),
                 )
-                for index, indexer in enumerate(indexers)
-                if indexer not in [None, last_indexer]
-            )
-        out_array = array(output)
-        for i, maybe_slice in enumerate(indexers):
-            if maybe_slice is None:
-                out_array[i] = nan
-        out_array[-1] = last_result
-        return out_array
+                Parallel(n_jobs=n_jobs)(
+                    delayed(_writes_output(applies_slice))(
+                        *new_args, _indexer=indexer, _output=output, _index=index, **new_kwargs,
+                    )
+                    for index, indexer in enumerate(indexers)
+                    if indexer not in [None, last_indexer]
+                )
+            out_array = array(output)
+            for i, maybe_slice in enumerate(indexers):
+                if maybe_slice is None:
+                    out_array[i] = nan
+            out_array[-1] = last_result
+            return out_array
 
-    return wrapped
+        return wrapped
