96c812cb1645f243ac424e7e980753218b49be9a
Derek Wan <d.wan@icloud.com>
2020-04-18 00:03:22 +0800

    Clean-ups

--- joblib_windower/__init__.py
+++ joblib_windower/__init__.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from joblib_windower.joblib_windower import arrays
+from joblib_windower.joblib_windower import windower
 
 
 __all__ = [
-    "arrays",
+    "windower",
 ]
 __version__ = "0.1.0"
